f15d51ac69bc1a3a6ab36d3f23a58e7f6101c179
Kieran <49917350+KRawl8@users.noreply.github.com>
2023-04-14 11:18:30 +0100

    Changed dist to not clear html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import "./style.css";
+// import { Todo } from "./todo";
 
 console.log("Hi");
+
+// let task1 = new Todo("Sleep", "Go to bed on time", "5th", "Top");
+// console.log(task1);
---
+++ src/todo.js
@@ -0,0 +1,8 @@
+export class Todo {
+  constructor(title, description, dueDate, priority) {
+    this._title = title;
+    this._description = description;
+    this._dueDate = dueDate;
+    this._prioroty = priority;
+  }
+}
